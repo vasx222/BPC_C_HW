4fe1b0e108ff63156cf98b2770d7e9d4d041d00b
vasx222 <vasx222@mail.ru>
2018-04-24 04:46:18 +0300

    added func where

--- main.c
+++ main.c
@@ -53,7 +53,7 @@ int assert_equals_ints(int a1, int a2) {
     return a1 == a2 ? OK : WRONG;
 }
 
-void test_OK() {
+void test_map_OK() {
     struct arr a1, a2;
     a1.len = 10;
     a1.data = (int*)malloc(a1.len * sizeof(int));
@@ -67,9 +67,11 @@ void test_OK() {
     int code = assert_equals(&a1, &a2);
     code = assert_equals_ints(code, OK);
     print_code(code);
+    free(a1.data);
+    free(a2.data);
 }
 
-void test_ERROR() {
+void test_map_ERROR() {
     struct arr a;
     a.len = 10;
     a.data = (int*)malloc(a.len * sizeof(int));
@@ -80,9 +82,10 @@ void test_ERROR() {
     int code = assert_equals(0, &a);
     code = assert_equals_ints(code, ERROR);
     print_code(code);
+    free(a.data);
 }
 
-void test_WRONG() {
+void test_map_WRONG() {
     struct arr a1, a2;
     a1.len = 10;
     a1.data = (int*)malloc(a1.len * sizeof(int));
@@ -96,11 +99,82 @@ void test_WRONG() {
     int code = assert_equals(&a1, &a2);
     code = assert_equals_ints(code, WRONG);
     print_code(code);
+    free(a1.data);
+    free(a2.data);
+}
+
+int condition(int * x) {
+    return *x > 7 ? OK : WRONG;
+}
+
+struct arr * where(struct arr * a, int (*func) (int*)) {
+    struct arr * res = (struct arr*)malloc(sizeof(struct arr));
+    res->len = a->len;
+    res->data = (int*)calloc((size_t) a->len, sizeof(int));
+
+    int len = 0;
+    for (int i = 0; i < a->len; i++) {
+        if (condition(&a->data[i]) == OK) {
+            res->data[len++] = a->data[i];
+        }
+    }
+    res->len = len;
+    if (len < a->len) {
+        res->data = (int*)realloc(res->data, len * sizeof(int));
+    }
+    return res;
+}
+
+void test_where_OK() {
+    struct arr a1, a2;
+    a1.len = 10;
+    a1.data = (int*)malloc(a1.len * sizeof(int));
+    a2.len = 3;
+    a2.data = (int*)malloc(a2.len * sizeof(int));
+    for (int i = 1; i <= a1.len; i++) {
+        a1.data[i - 1] = i;
+    }
+    a2.data[0] = 8;
+    a2.data[1] = 9;
+    a2.data[2] = 10;
+    struct arr * res = where(&a1, condition);
+    int code = assert_equals(res, &a2);
+    code = assert_equals_ints(code, OK);
+    print_code(code);
+    free(res);
+    free(a1.data);
+    free(a2.data);
+}
+
+int condition2(int * x) {
+    return *x % 2 == 0 ? OK : WRONG;
+}
+
+void test_where_WRONG() {
+    struct arr a1, a2;
+    a1.len = 10;
+    a1.data = (int*)malloc(a1.len * sizeof(int));
+    a2.len = 10;
+    a2.data = (int*)malloc(a2.len * sizeof(int));
+    for (int i = 1; i <= a1.len; i++) {
+        a1.data[i - 1] = i;
+        a2.data[i - 1] = i * 2;
+    }
+    struct arr * res = where(&a1, condition2);
+    int code = assert_equals(res, &a2);
+    code = assert_equals_ints(code, WRONG);
+    print_code(code);
+    free(res);
+    free(a1.data);
+    free(a2.data);
 }
 
 int main() {
-    test_OK();
-    test_ERROR();
-    test_WRONG();
+    test_map_OK();
+    test_map_ERROR();
+    test_map_WRONG();
+
+    test_where_OK();
+    test_where_WRONG();
     return 0;
 }
